9397f30a86180ea5f8f71d741ac4af8b27ea859e
sruhshona2197 <sruhshona2197@gmail.com>
2025-11-01 17:25:28 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# 1
+son = (34, 97, 1, 4)
+yigin = sum(son)
+print(yigin)
+
+
+#-4
+royhat = (11, 54, 26, 77, 31, 10)
+j_son = ()
+for son in royhat:
+    if son % 2 == 0:
+         son += (son,)
+
+print(j_son)
+
+# -2
+royhat = (3, 1,7, 15, 12)
+son = max(royhat)
+print(son)
+
+# 3
+son1 = (1, 2, 3)
+son2 = (4, 5, 6)
+royhat = son1 + son2
+print(royhat)
+
+# 5
+royhat = (1, 5, 2, 8, 9)
+son = royhat[::-1]
+print(son)
